0b08da0f3c9406f2902e42c9183d58feb4df4aee
boufala <lamia.boufala@eud.univ-paris13.fr>
2024-05-30 18:21:00 +0200

    done with fragement 3

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-2*10+x == 3*8;
+// to test the if else
+if (x<10){
+    x+1;
+}else{
+    y=10;
+}
+// to test the do while
+do {
+    y=y+1;
+}while(y<10);
+
+// to test the compressed sequence of commands
+
+{
+    e+1;
+    a-10;
+    3+1;
+}
+// to test the EmptyCommand
+;
+    
