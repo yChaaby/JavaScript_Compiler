dc6a69a1b13bd396d668bf782e45077ff1991a67
boufala <lamia.boufala@eud.univ-paris13.fr>
2024-06-03 08:29:27 +0200

    un petit teste

--- test.js
+++ test.js
@@ -1,12 +1,18 @@
-/*function factorial(n) {
+function factorial(n) {
     if (n <= 1) return 1;
     else return n * factorial(n - 1);
+
+    try{
+        x=1;
+    }catch (e) {
+        5;
+    }
 }
 p=3;
 x=factorial(p);
 x=-(x*(p+1));
-y=x+((x+false)&&3);*/
-throw 1+1;
+y=x+((x+false)&&3);
+
 
 
 
